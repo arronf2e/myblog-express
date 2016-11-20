6e656e727cf865b47b501e234eb783d26cdcdef5
njarron <hao.zhang@baoz.cn>
2016-11-20 23:10:50 +0800

    update

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ var checkLogin = require('../middlewares/check').checkLogin;
 
 // GET /posts 所有用户或者特定用户的文章页
 router.get('/', function(req,res,next) {
-    res.send(req.flash())
+    res.render('posts')
 });
 
 // POST /posts 发表一篇文章
